7fa6497ff0ffa1ba1370657f730386a5a30153f8
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-06 21:02:16 +0530

    Ability To Edit Contact

---
+++ src/com/magic/addressbook/AddressBook.java
@@ -0,0 +1,51 @@
+package com.magic.addressbook;
+import java.util.List;
+import java.util.*;
+import java.util.ArrayList;
+
+public class AddressBook {
+    static List<AddressBookSystemManage> contactList=new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+
+    public void addContact(){
+        System.out.println("Enter First Name");
+        String firstName=input.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName=input.nextLine();
+        System.out.println("Enter Address");
+        String address=input.nextLine();
+        System.out.println("Enter city");
+        String city=input.nextLine();
+        System.out.println("Enter State");
+        String state=input.nextLine();
+        System.out.println("Enter Phone Number");
+        String phoneNumber=input.nextLine();
+        System.out.println("Enter Zip");
+        String zip=input.nextLine();
+        System.out.println("Enter Email");
+        String email=input.nextLine();
+        AddressBookSystemManage addressBook = new AddressBookSystemManage(firstName,lastName,address,city,state,phoneNumber,zip,email);
+        contactList.add(addressBook);
+
+    }
+    public void editContact(){
+        Scanner nameInput = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName = nameInput.nextLine();
+        for(int i = 0; i < contactList.size(); i++){
+            if (contactList.get(i).getFirstName().equals(firstName)){
+                contactList.remove(i);
+                AddressBook addressBook = new AddressBook();
+                addressBook.addContact();
+            }
+            else{
+                System.out.println("No Contact");
+            }
+
+        }
+    }
+
+
+
+
+}
--- src/com/magic/addressbook/AddressBookSystem.java
+++ src/com/magic/addressbook/AddressBookSystem.java
@@ -1,39 +1,36 @@
 package com.magic.addressbook;
-import javax.swing.*;
-import java.util.List;
-import java.util.*;
-import java.util.ArrayList;
+
+import java.util.Scanner;
 
 public class AddressBookSystem {
-    static List<AddressBookSystemManage> contactList=new ArrayList<>();
-    static Scanner input = new Scanner(System.in);
-
-    public static void addContact(){
-        System.out.println("Enter First Name");
-        String firstName=input.nextLine();
-        System.out.println("Enter Last Name");
-        String lastName=input.nextLine();
-        System.out.println("Enter Address");
-        String address=input.nextLine();
-        System.out.println("Enter city");
-        String city=input.nextLine();
-        System.out.println("Enter State");
-        String state=input.nextLine();
-        System.out.println("Enter Phone Number");
-        String phoneNumber=input.nextLine();
-        System.out.println("Enter Zip");
-        String zip=input.nextLine();
-        System.out.println("Enter Email");
-        String email=input.nextLine();
-        AddressBookSystemManage addressBook = new AddressBookSystemManage(firstName,lastName,address,city,state,phoneNumber,zip,email);
-        contactList.add(addressBook);
-
-    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome To the AddressBook");
-        addContact();
-        System.out.println(contactList);
-
+        AddressBook addressBook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+        int flag = 1 ;
+        while(flag == 1)
+        {
+            System.out.println(" Welcome to address book program ");
+            System.out.println(" Select a choice : 1. Add 2.Edit  3. Exit");
+            int choice = sc.nextInt();
+            switch(choice)
+            {
+                case 1 : addressBook.addContact();
+                    //System.out.println(addressBook.contactList());
+                    break;
+                case 2 : if (addressBook.contactList.isEmpty())
+                {
+                    System.out.println(" Address Book is empty ");
+                    break;
+                }
+                    addressBook.editContact();
+                    break;
+                case 3 : flag = 0 ;
+                    break;
+                default: System.out.println(" Enter a valid choice");
+                    break;
+            }
+            System.out.println(addressBook.contactList);
+        }
     }
 }
