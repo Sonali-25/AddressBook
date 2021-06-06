0e4710f69adf897b5382d789f847f83ca5dfb795
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-06 16:24:03 +0530

    Welcome to the Address Book

---
+++ src/com/magic/addressbook/AddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.magic.addressbook;
+
+public class AddressBookSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To the AddressBook");
+
+    }
+}
