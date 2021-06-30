96ccbdc856f3b0ab53351e567d73f734e30e99c0
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-06-30 22:39:47 +0530

    Ability To Retrieve Records From DataBase

---
+++ src/main/java/com/magic/addressbookdb/AddressbookRepo.java
@@ -0,0 +1,51 @@
+package com.magic.addressbookdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressbookRepo {
+    public List<Contact> findAll() throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        List<Contact> details = new ArrayList<>();
+        try {
+            DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook_service", "root", "root");
+            statement = connection.createStatement();
+            String query = "Select * from addressbook";
+            ResultSet result = statement.executeQuery(query);
+            while (result.next()) {
+                Contact info = new Contact();
+                String firstName = result.getString(1);
+                info.setFirstName(firstName);
+                String lastName = result.getString(2);
+                info.setLastName(lastName);
+                String address = result.getString(3);
+                info.setAddress(address);
+                String city = result.getString(4);
+                info.setCity(city);
+                String phoneNo = result.getString(5);
+                info.setPhoneNo(phoneNo);
+                details.add(info);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+        }
+        return details;
+    }
+}
---
+++ src/main/java/com/magic/addressbookdb/Addressbook_Service.java
@@ -0,0 +1,31 @@
+package com.magic.addressbookdb;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Addressbook_Service
+{
+    static Scanner sc =new Scanner(System.in);
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
+    {
+        System.out.println("Select option");
+        System.out.println("1. Retrieve All Records ");
+        int option = sc.nextInt();
+        switch(option) {
+            case 1:
+                printData();
+                break;
+        }
+    }
+
+    private static void printData() throws ClassNotFoundException, SQLException {
+        AddressbookRepo addressbookRepo = new AddressbookRepo();
+        List<Contact> detailList = addressbookRepo.findAll();
+        detailList.forEach(value -> System.out.println(value));
+    }
+}
+
+
+  
+
