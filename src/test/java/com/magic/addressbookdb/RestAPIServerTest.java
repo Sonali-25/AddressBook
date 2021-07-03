06ffce349e30e2ea7270467ac2f247d9aad8ea7e
Sonali265 <Sonalibhardwajhjp@gmail.com>
2021-07-03 13:02:40 +0530

    Ability To Add Multiple Entries Json Server

--- src/test/java/com/magic/addressbookdb/RestAPIServerTest.java
+++ src/test/java/com/magic/addressbookdb/RestAPIServerTest.java
@@ -1,6 +1,10 @@
 package com.magic.addressbookdb;
 
+import io.restassured.http.ContentType;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
+import org.testing.annotations.DataProvider;
+import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.*;
 
@@ -12,6 +16,66 @@ public class RestAPIServerTest {
                 get("http://localhost:3000/Contacts").
                 then().
                 statusCode(200).
+                log().all();
+    }
+    @DataProvider(name = "dataForPost")
+    public Object[][] dataForPost() {
+        return new Object[][] {
+                {"Mark","Smith","WestVilla","Kentucky","NewStates","9000056102","mark123@gmail.com"},
+                {"Sam","Sam","OxfordRoad","Vegas","LasVegas","990156102","sam@outlook.com"}
+        };
+    }
+
+    @Test(dataProvider = "dataForPost")
+    public void addMultipleRecords_shouldReturn_201statusCode(String firstName, String lastName, String address, String city, String state, String phoneNumber ,String emailId) {
+        JSONObject request = new JSONObject();
+
+        request.put("FirstName", firstName);
+        request.put("LastName",  lastName);
+        request.put("Address", address);
+        request.put("City", city);
+        request.put("State", state);
+        request.put("PhoneNumber", phoneNumber);
+        request.put("EmailId", emailId);
+
+        baseURI ="http://localhost";
+        port = 3000;
+
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                header("Content-Type", "application/json").
+                body(request.toJSONString()).
+                when().
+                post("/Contacts").
+                then().
+                statusCode(201).
+                log().all();
+    }
+    @Test
+    public void addSingleRecords_shouldReturn_201statusCode() {
+        JSONObject request = new JSONObject();
+
+        request.put("FirstName", "Nikita");
+        request.put("LastName",  "Pardhi");
+        request.put("Address", "Tilak Nagar");
+        request.put("City","Nagpur");
+        request.put("State", "MH");
+        request.put("PhoneNumber", "13254678");
+        request.put("EmailId", "nikita@rediffmail.com");
+
+        baseURI ="http://localhost";
+        port = 3000;
+
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                header("Content-Type", "application/json").
+                body(request.toJSONString()).
+                when().
+                post("/Contacts").
+                then().
+                statusCode(201).
                 log().all();
     }
 }
